9ccf12bd8b297030412cd00af1f6517955a79ce0
Christina Xu <cjxu@bu.edu>
2022-09-15 16:47:38 -0400

    add park

--- labs/01-lab/outdoors/park.py
+++ labs/01-lab/outdoors/park.py
@@ -1,3 +1,14 @@
 def draw_park():
-    print("park not found")
-    return
+    row = 12
+    col = 12
+    for i in range(1, row+1):
+        for j in range(1, col+1) :
+            if (i == 1 or i == row or
+                j == 1 or j == col) :
+                print("*", end="")           
+            else :
+                print(" ", end="")           
+        print()
+
+draw_park()
+
