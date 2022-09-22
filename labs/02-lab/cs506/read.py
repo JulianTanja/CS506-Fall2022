b9fad4cdb945f4e8666f02b0d0b0819d73ec2f2d
JulianTanja <61524227+JulianTanja@users.noreply.github.com>
2022-09-22 17:15:22 -0400

    lab2 finish

--- labs/02-lab/cs506/read.py
+++ labs/02-lab/cs506/read.py
@@ -1,6 +1,15 @@
+import csv
+
+
 def read_csv(csv_file_path):
     """
         Given a path to a csv file, return a matrix (list of lists)
         in row major.
     """
-    raise NotImplementedError()
+
+    with open(csv_file_path, 'r') as file:
+        csvreader = csv.reader(file)
+        for row in csvreader:
+            print(row)
+
+    #raise NotImplementedError()
--- labs/02-lab/cs506/sim.py
+++ labs/02-lab/cs506/sim.py
@@ -1,16 +1,38 @@
+import math
+
+
 def euclidean_dist(x, y):
     res = 0
     for i in range(len(x)):
         res += (x[i] - y[i])**2
     return res**(1/2)
 
+
 def manhattan_dist(x, y):
-    raise NotImplementedError()
+    return sum(abs(x-y) for x, y in zip(x, y))
+
+    #raise NotImplementedError()
+
 
 def jaccard_dist(x, y):
-    raise NotImplementedError()
+    intersection = len(list(set(x).intersection(y)))
+    union = (len(x) + len(y)) - intersection
+    jaccard_sim = float(intersection)/union
+    ans = 1 - jaccard_sim
+    return ans
+    #raise NotImplementedError()
+
 
 def cosine_sim(x, y):
-    raise NotImplementedError()
+    # Similarity = (A.B) / (||A||.||B||)
+
+    dot = sum([ix*iy for (ix, iy) in zip(x, y)])
+    normX = math.sqrt(sum(pow(element, 2) for element in x))
+    normY = math.sqrt(sum(pow(element, 2) for element in y))
+
+    dotDenom = sum([vx*vy] for (vx, vy) in zip(normX, normY))
+    cosine_simil = dot / dotDenom
+    return cosine_simil
+    #raise NotImplementedError()
 
 # Feel free to add more
